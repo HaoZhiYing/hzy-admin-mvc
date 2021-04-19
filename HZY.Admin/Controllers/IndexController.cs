736336a46f455353907cfeec250023d00efdbc60
hzy <1396510655@qq.com>
2021-04-19 21:51:02 +0800

    忽略接口

--- HZY.Admin/Controllers/IndexController.cs
+++ HZY.Admin/Controllers/IndexController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace HZY.Admin.Controllers
 {
+    [ApiExplorerSettings(IgnoreApi = true)]
     public class IndexController : Controller
     {
         [Route("")]

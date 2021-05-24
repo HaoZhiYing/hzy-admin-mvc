fb63de39ac0c44d42850353bdc652467f5a4def5
hzy6 <1396510655@qq.com>
2021-05-24 11:03:05 +0800

    添加注释提示

--- HZY.Repository/AppCore/RepositoryModule.cs
+++ HZY.Repository/AppCore/RepositoryModule.cs
@@ -8,9 +8,17 @@ using Microsoft.Extensions.Logging;
 
 namespace HZY.Repository.AppCore
 {
+    /// <summary>
+    /// 仓储模块
+    /// </summary>
     public class RepositoryModule
     {
-
+        /// <summary>
+        /// 注册 Admin 后台管理数据库
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="connectionString">默认数据库连接字符串</param>
+        /// <param name="defaultDatabaseType">默认数据库类型</param>
         public static void RegisterAdminRepository(IServiceCollection services, string connectionString, DefaultDatabaseType defaultDatabaseType)
         {
             #region 后台 管理系统 数据库上下文

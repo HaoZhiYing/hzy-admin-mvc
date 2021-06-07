c4d70a030f8de5ac3abda9f605e127691b891b8b
hzy <1396510655@qq.com>
2021-06-07 20:05:15 +0800

    删除多余的文件

--- HZY.Repository/Framework/SysOrganizationRepository.cs
+++ HZY.Repository/Framework/SysOrganizationRepository.cs
@@ -1,12 +1,6 @@
-﻿using HZY.Common;
-using HZY.Repository.AppCore.DbContexts;
+﻿using HZY.Repository.AppCore.DbContexts;
 using HZY.Repository.AppCore.Provider;
 using HZY.Repository.Domain.Framework;
-using HZY.Repository.Dto;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace HZY.Repository.Framework
 {

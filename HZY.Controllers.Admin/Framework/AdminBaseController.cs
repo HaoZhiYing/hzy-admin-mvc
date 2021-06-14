664be6e3a1d324c0e009fb2d2922bf7ff987a4ab
hzy <1396510655@qq.com>
2021-06-14 19:30:10 +0800

    添加注释

--- HZY.Controllers.Admin/Framework/AdminBaseController.cs
+++ HZY.Controllers.Admin/Framework/AdminBaseController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HZY.Controllers.Admin.Framework
 {
-    [Authorize]
+    [Authorize]//是否授权 Authorize
     [ApiExplorerSettings(GroupName = nameof(ApiVersions.Admin_后台权限管理系统_Api))]
     [Route("api/admin/[controller]")]
     public class AdminBaseController<TDefaultService> : ApiBaseController<TDefaultService> where TDefaultService : class
@@ -16,7 +16,7 @@ namespace HZY.Controllers.Admin.Framework
         }
     }
 
-    [Authorize]
+    [Authorize]//是否授权 Authorize
     [ApiExplorerSettings(GroupName = nameof(ApiVersions.Admin_后台权限管理系统_Api))]
     [Route("api/admin/[controller]")]
     public class AdminBaseController : ApiBaseController
